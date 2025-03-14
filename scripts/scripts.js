b3560419e5cc7d9f8c88e22a4689a45180211add
EllieColorado <123810583+EllieColorado@users.noreply.github.com>
2025-03-14 17:06:59 +0100

    Versión 2 (el nav se ha extendido a lo ancho de la página, se ha cambiado cómo se muestran los proyectos, se han añadido imágenes de proyectos, empresas y organizaciones...)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,23 @@
 function openTab(tabId) {
-    // Ocultar todas las pestañas
     const tabs = document.querySelectorAll('.tab-content');
     tabs.forEach(tab => {
         tab.classList.remove('active');
+        tab.style.animation = 'none';
     });
 
-    // Quitar la clase active de todos los enlaces
     const links = document.querySelectorAll('nav a');
     links.forEach(link => {
         link.classList.remove('active');
     });
 
-    // Mostrar la pestaña seleccionada
     const selectedTab = document.getElementById(tabId);
     selectedTab.classList.add('active');
+    selectedTab.style.animation = '';
 
-    // Añadir la clase active al enlace seleccionado
     const selectedLink = document.querySelector(`nav a[onclick="openTab('${tabId}')"]`);
     selectedLink.classList.add('active');
 }
 
-// Mostrar la primera pestaña por defecto
 document.addEventListener('DOMContentLoaded', () => {
-    openTab('experience');
+    openTab('about-me');
 });
